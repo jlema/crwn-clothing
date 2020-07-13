e1123b486a718335973eab4125c8af23afa94ed9
John (Juan) Lema <jlema@users.noreply.github.com>
2020-07-12 22:07:00 -0700

    Updates

---
+++ precache-manifest.ff24f0ade53dbb72ff7233c11486ece2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa547a7eaea96e74dbc0a072b2e9fb9",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "2a11881de3a0a16ea6ce",
+    "url": "/crwn-clothing/static/css/main.26cea919.chunk.css"
+  },
+  {
+    "revision": "0e69fe7a1cf3b9bc2c63",
+    "url": "/crwn-clothing/static/js/2.c7e1c575.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crwn-clothing/static/js/2.c7e1c575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a11881de3a0a16ea6ce",
+    "url": "/crwn-clothing/static/js/main.aa83ea7c.chunk.js"
+  },
+  {
+    "revision": "8558d1d6d2663c53b558",
+    "url": "/crwn-clothing/static/js/runtime-main.742f8efe.js"
+  }
+]);
