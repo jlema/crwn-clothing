30849cea64aaaf8cfa9b49ffcb381adcfc2f1ddd
John (Juan) Lema <jlema@users.noreply.github.com>
2020-07-20 21:46:43 -0700

    added redux, user reducer and actions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
-	<BrowserRouter basename={`${process.env.PUBLIC_URL}/`}>
-		<App />
-	</BrowserRouter>,
+	<Provider store={store}>
+		<BrowserRouter basename={`${process.env.PUBLIC_URL}/`}>
+			<App />
+		</BrowserRouter>
+	</Provider>,
 	document.getElementById('root')
 );
