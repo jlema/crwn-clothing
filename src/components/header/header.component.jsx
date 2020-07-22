0fe6c6b77786b124cf55eabc355e3039a7ac0078
John (Juan) Lema <jlema@users.noreply.github.com>
2020-07-21 22:54:51 -0700

    Added add to cart button to collection item Added hover styles to custom button and collection item

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,7 +5,6 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 import { auth } from '../../firebase/firebase.utils';
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './header.styles.scss';
 
